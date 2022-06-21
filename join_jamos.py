2991e0b19e804651053ea7167d6fb54c45e43724
eunsour <woo569628@naver.com>
2022-06-21 16:27:10 +0900

    update jamo

--- join_jamos.py
+++ join_jamos.py
@@ -1,6 +1,12 @@
-__all__ = ["split_syllable_char", "split_syllables",
-           "join_jamos", "join_jamos_char",
-           "CHAR_INITIALS", "CHAR_MEDIALS", "CHAR_FINALS"]
+__all__ = [
+    "split_syllable_char",
+    "split_syllables",
+    "join_jamos",
+    "join_jamos_char",
+    "CHAR_INITIALS",
+    "CHAR_MEDIALS",
+    "CHAR_FINALS",
+]
 
 import itertools
 
@@ -8,61 +14,131 @@ INITIAL = 0x001
 MEDIAL = 0x010
 FINAL = 0x100
 CHAR_LISTS = {
-    INITIAL: list(map(chr, [
-        0x3131, 0x3132, 0x3134, 0x3137, 0x3138, 0x3139,
-        0x3141, 0x3142, 0x3143, 0x3145, 0x3146, 0x3147,
-        0x3148, 0x3149, 0x314a, 0x314b, 0x314c, 0x314d,
-        0x314e
-    ])),
-    MEDIAL: list(map(chr, [
-        0x314f, 0x3150, 0x3151, 0x3152, 0x3153, 0x3154,
-        0x3155, 0x3156, 0x3157, 0x3158, 0x3159, 0x315a,
-        0x315b, 0x315c, 0x315d, 0x315e, 0x315f, 0x3160,
-        0x3161, 0x3162, 0x3163
-    ])),
-    FINAL: list(map(chr, [
-        0x3131, 0x3132, 0x3133, 0x3134, 0x3135, 0x3136,
-        0x3137, 0x3139, 0x313a, 0x313b, 0x313c, 0x313d,
-        0x313e, 0x313f, 0x3140, 0x3141, 0x3142, 0x3144,
-        0x3145, 0x3146, 0x3147, 0x3148, 0x314a, 0x314b,
-        0x314c, 0x314d, 0x314e
-    ]))
+    INITIAL: list(
+        map(
+            chr,
+            [
+                0x3131,
+                0x3132,
+                0x3134,
+                0x3137,
+                0x3138,
+                0x3139,
+                0x3141,
+                0x3142,
+                0x3143,
+                0x3145,
+                0x3146,
+                0x3147,
+                0x3148,
+                0x3149,
+                0x314A,
+                0x314B,
+                0x314C,
+                0x314D,
+                0x314E,
+            ],
+        )
+    ),
+    MEDIAL: list(
+        map(
+            chr,
+            [
+                0x314F,
+                0x3150,
+                0x3151,
+                0x3152,
+                0x3153,
+                0x3154,
+                0x3155,
+                0x3156,
+                0x3157,
+                0x3158,
+                0x3159,
+                0x315A,
+                0x315B,
+                0x315C,
+                0x315D,
+                0x315E,
+                0x315F,
+                0x3160,
+                0x3161,
+                0x3162,
+                0x3163,
+            ],
+        )
+    ),
+    FINAL: list(
+        map(
+            chr,
+            [
+                0x3131,
+                0x3132,
+                0x3133,
+                0x3134,
+                0x3135,
+                0x3136,
+                0x3137,
+                0x3139,
+                0x313A,
+                0x313B,
+                0x313C,
+                0x313D,
+                0x313E,
+                0x313F,
+                0x3140,
+                0x3141,
+                0x3142,
+                0x3144,
+                0x3145,
+                0x3146,
+                0x3147,
+                0x3148,
+                0x314A,
+                0x314B,
+                0x314C,
+                0x314D,
+                0x314E,
+            ],
+        )
+    ),
 }
 CHAR_INITIALS = CHAR_LISTS[INITIAL]
 CHAR_MEDIALS = CHAR_LISTS[MEDIAL]
 CHAR_FINALS = CHAR_LISTS[FINAL]
 CHAR_SETS = {k: set(v) for k, v in CHAR_LISTS.items()}
 CHARSET = set(itertools.chain(*CHAR_SETS.values()))
-CHAR_INDICES = {k: {c: i for i, c in enumerate(v)}
-                for k, v in CHAR_LISTS.items()}
+CHAR_INDICES = {k: {c: i for i, c in enumerate(v)} for k, v in CHAR_LISTS.items()}
 
 
 def is_hangul_syllable(c):
-    return 0xac00 <= ord(c) <= 0xd7a3  # Hangul Syllables
+    return 0xAC00 <= ord(c) <= 0xD7A3  # Hangul Syllables
 
 
 def is_hangul_jamo(c):
-    return 0x1100 <= ord(c) <= 0x11ff  # Hangul Jamo
+    return 0x1100 <= ord(c) <= 0x11FF  # Hangul Jamo
 
 
 def is_hangul_compat_jamo(c):
-    return 0x3130 <= ord(c) <= 0x318f  # Hangul Compatibility Jamo
+    return 0x3130 <= ord(c) <= 0x318F  # Hangul Compatibility Jamo
 
 
 def is_hangul_jamo_exta(c):
-    return 0xa960 <= ord(c) <= 0xa97f  # Hangul Jamo Extended-A
+    return 0xA960 <= ord(c) <= 0xA97F  # Hangul Jamo Extended-A
 
 
 def is_hangul_jamo_extb(c):
-    return 0xd7b0 <= ord(c) <= 0xd7ff  # Hangul Jamo Extended-B
+    return 0xD7B0 <= ord(c) <= 0xD7FF  # Hangul Jamo Extended-B
 
 
 def is_hangul(c):
-    return (is_hangul_syllable(c) or
-            is_hangul_jamo(c) or
-            is_hangul_compat_jamo(c) or
-            is_hangul_jamo_exta(c) or
-            is_hangul_jamo_extb(c))
+    return (
+        is_hangul_syllable(c)
+        or is_hangul_jamo(c)
+        or is_hangul_compat_jamo(c)
+        or is_hangul_jamo_exta(c)
+        or is_hangul_jamo_extb(c)
+    )
 
 
 def is_supported_hangul(c):
@@ -71,10 +147,12 @@ def is_supported_hangul(c):
 
 def check_hangul(c, jamo_only=False):
     if not ((jamo_only or is_hangul_compat_jamo(c)) or is_supported_hangul(c)):
-        raise ValueError(f"'{c}' is not a supported hangul character. "
-                         f"'Hangul Syllables' (0xac00 ~ 0xd7a3) and "
-                         f"'Hangul Compatibility Jamos' (0x3130 ~ 0x318f) are "
-                         f"supported at the moment.")
+        raise ValueError(
+            f"'{c}' is not a supported hangul character. "
+            f"'Hangul Syllables' (0xac00 ~ 0xd7a3) and "
+            f"'Hangul Compatibility Jamos' (0x3130 ~ 0x318f) are "
+            f"supported at the moment."
+        )
 
 
 def get_jamo_type(c):
@@ -108,14 +186,14 @@ def split_syllable_char(c):
 
     init, med, final = None, None, None
     if is_hangul_syllable(c):
-        offset = ord(c) - 0xac00
+        offset = ord(c) - 0xAC00
         x = (offset - offset % 28) // 28
         init, med, final = x // 21, x % 21, offset % 28
-        
-        print(f'init : {init}')
-        print(f'med : {med}')
-        print(f'final : {final}')
-        
+
+        print(f"init : {init}")
+        print(f"med : {med}")
+        print(f"final : {final}")
+
         if not final:
             final = None
         else:
@@ -135,9 +213,10 @@ def split_syllable_char(c):
             med = idx
         else:
             final = idx
-    return tuple(CHAR_LISTS[pos][idx] if idx is not None else None
-                 for pos, idx in
-                 zip([INITIAL, MEDIAL, FINAL], [init, med, final]))
+    return tuple(
+        CHAR_LISTS[pos][idx] if idx is not None else None
+        for pos, idx in zip([INITIAL, MEDIAL, FINAL], [init, med, final])
+    )
 
 
 def split_syllables(s, ignore_err=True, pad=None):
@@ -168,8 +247,9 @@ def split_syllables(s, ignore_err=True, pad=None):
         except ValueError:
             if ignore_err:
                 return (c,)
-            raise ValueError(f"encountered an unsupported character: "
-                             f"{c} (0x{ord(c):x})")
+            raise ValueError(
+                f"encountered an unsupported character: " f"{c} (0x{ord(c):x})"
+            )
 
     s = map(try_split, s)
     if pad is not None:
@@ -194,13 +274,15 @@ def join_jamos_char(init, med, final=None):
     for c in filter(None, chars):
         check_hangul(c, jamo_only=True)
 
-    idx = tuple(CHAR_INDICES[pos][c] if c is not None else c
-                for pos, c in zip((INITIAL, MEDIAL, FINAL), chars))
+    idx = tuple(
+        CHAR_INDICES[pos][c] if c is not None else c
+        for pos, c in zip((INITIAL, MEDIAL, FINAL), chars)
+    )
     init_idx, med_idx, final_idx = idx
     # final index must be shifted once as
     # final index with 0 points to syllables without final
     final_idx = 0 if final_idx is None else final_idx + 1
-    return chr(0xac00 + 28 * 21 * init_idx + 28 * med_idx + final_idx)
+    return chr(0xAC00 + 28 * 21 * init_idx + 28 * med_idx + final_idx)
 
 
 def join_jamos(s, ignore_err=True):
