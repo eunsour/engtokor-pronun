3a8b769ffa7b9667ab36e2b4b9f1501a3174c3ac
eunsour <woo569628@naver.com>
2022-08-31 23:23:07 +0900

    modify oxford crawling

--- oxford_crawling.py
+++ oxford_crawling.py
@@ -2,18 +2,24 @@ import requests
 
 from scrapy import Selector
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
-}
+
+def get_resp(eng_word):
+    base_url = f"https://www.oxfordlearnersdictionaries.com/definition/english/{eng_word}?q={eng_word}"
+    payload = {}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
+    }
+
+    response = requests.request("GET", base_url, headers=headers, data=payload)
+
+    return response
+
 
 def oxford_crawling(word):
-    base_url = (
-        f"https://www.oxfordlearnersdictionaries.com/definition/english/{word}?q={word}"
-    )
-    resp = requests.get(base_url, headers=headers)
+    us_pronun = None
+    resp = get_resp(word)
     sel = Selector(resp)
 
-    us = None
     container_list = sel.xpath('//div[@class="webtop"]/span[@class="phonetics"]')
 
     for container in container_list:
@@ -24,15 +30,16 @@ def oxford_crawling(word):
             .replace("/", "")
         )
         """
-        us = (
+        us_pronun = (
             container.xpath('div[@class="phons_n_am"]/span[@class="phon"]/text()')
             .get()
             .replace("/", "")
         )
 
-    return us
+    return us_pronun
 
 
 if __name__ == "__main__":
     word = "transformer"
-    print(oxford_crawling(word=word))
+
+    print(oxford_crawling(word))
